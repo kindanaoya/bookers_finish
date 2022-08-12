72e976337641606752f159756e881a3fff4033eb
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-08-12 13:16:25 +0000

    [Add]book新規登録機能、bookのcreatee機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def index
@@ -10,5 +17,9 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
