7b07c8f16016728c2e81ba7d87fb73eb01a76d58
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-08-15 14:49:05 +0000

    [update]destroy機能の編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,14 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to show_book_path(@book.id)
     else
       render :new
     end
   end
 
   def index
+    @book = Book.new 
     @books = Book.all
   end
 
@@ -29,16 +30,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
        flash[:notice] = "Book was successfully updated."
-       redirect_to book_path(@book.id)
+       redirect_to show_book_path(@book.id)
     else
       render :edit
     end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    @book = Book.find(params[:id])
+    if @book.destroy(book_params)
+      flash[:notice] = "Book was successfully destroyed"
+      redirect_to 'books'
+    end
   end
   
   private
