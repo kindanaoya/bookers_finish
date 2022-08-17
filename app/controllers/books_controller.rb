c4724e08f412286d9bb0ca7f02dfdcc63b208bbf
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-08-17 06:25:37 +0000

    [Add]resourcesメソッドへの書き換え

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to show_book_path(@book.id)
+      redirect_to book_path(@book.id)
     else
-      render :new
-
+      @books = Book.all
+      render :index
     end
   end
 
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
        flash[:notice] = "Book was successfully updated."
-       redirect_to show_book_path(@book.id)
+       redirect_to book_path(@book.id)
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     if book.destroy
       flash[:notice] = "Book was successfully destroyed"
-      redirect_to index_book_path
+      redirect_to books_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get '/' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index', as: 'index_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'books/:id/show' => 'books#show', as: 'show_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy',  as: 'destroy_book'
-  
+  root :to => 'homes#top'
+ # post 'books' => 'books#create'
+ # get 'books' => 'books#index', as: 'index_book'
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #get 'books/:id/show' => 'books#show', as: 'show_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy',  as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
